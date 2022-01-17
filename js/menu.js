66c3efed05b664207634bcee611a2600b97b7b97
Venegas20 <97493330+Venegas20@users.noreply.github.com>
2022-01-16 19:37:06 -0500

    pagina casi terminado
    webbootstrap

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,18 @@ var toastLiveExample = document.getElementById('liveToast')
 if (toastTrigger) {
   toastTrigger.addEventListener('click', function () {
     var toast = new bootstrap.Toast(toastLiveExample)
-
     toast.show()
   })
+
 }
+/*  */
+$(document).ready(function () {
+ /* 
+ * esta funcion hace que cierre el menu  en tamano movil 
+ */
+  $('.BotonCerrarMenu').click(function (e) { 
+    $('#navbarSupportedContent').toggleClass('show');
+    
+    
+  });
+});
